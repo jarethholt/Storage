b6bf92fa254742a6dd772bfb60f1a2bf01b05389
jarethholt <jareth.holt@gmail.com>
2024-04-10 11:10:53 +0200

    feat: Add data annotations to Product properties

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,13 +1,31 @@
-﻿namespace Storage.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
-public class Product
+namespace Storage.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Price { get; set; }
-    public DateTime Orderdate { get; set; }
-    public string Category { get; set; }
-    public string Shelf { get; set; }
-    public int Count { get; set; }
-    public string Description { get; set; }
+    public class Product
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(0, 1_000_000)]
+        public int Price { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Orderdate { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string Category { get; set; } = string.Empty;
+        [Required]
+        [StringLength(10, MinimumLength = 1)]
+        public string Shelf { get; set; } = string.Empty;
+        [Range(0, 1_000)]
+        public int Count { get; set; }
+        [StringLength(255)]
+        public string Description { get; set; } = string.Empty;
+    }
 }
